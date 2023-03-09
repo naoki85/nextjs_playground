1ff3d747e9af9c87bf474d599f791356e92858b1
naoki85 <naoki.yoneyama.85@gmail.com>
2023-03-09 14:48:16 +0900

    install typeorm

---
+++ app_api/src/data-source.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    synchronize: true,
+    logging: true,
+    entities: ["./entity/*.ts"],
+    migrations: ["./migration/*.ts"],
+    subscribers: [],
+});
