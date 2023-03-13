45c40fe7d7a42536afef5f4bf9b714cf233f70b7
naoki85 <naoki.yoneyama.85@gmail.com>
2023-03-13 14:42:17 +0900

    create api request methods

---
+++ front-app/apps/frontend/src/app/api/index.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
+
+export type User = {
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    mail: string;
+    password?: string;
+};
+
+export const signUp = async (data: User) => {
+    await axios.post("/signup", data);
+    return;
+};
+
+export const login = async (data: User) => {
+    await axios.post("/login", data);
+    return;
+};
+
+export const logout = async () => {
+    await axios.delete("/logout");
+    return;
+};
+
+export const validateJwtToken = async () => {
+    const response = await axios.get("/tokenVerification");
+    return response;
+};
