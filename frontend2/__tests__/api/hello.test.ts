21990b4ca030483fd9fef68aff95c5069a1213a8
naoki85 <naoki.yoneyama.85@gmail.com>
2023-03-19 13:26:53 +0900

    install jest

---
+++ frontend2/__tests__/api/hello.test.ts
@@ -0,0 +1,14 @@
+import httpMocks from 'node-mocks-http';
+import { NextApiRequest, NextApiResponse } from 'next';
+import handler from '../../pages/api/hello';
+
+describe('/api/hello handler', () => {
+  test('responds 200 to GET', async () => {
+    const req = httpMocks.createRequest<NextApiRequest>({});
+    const res = httpMocks.createResponse<NextApiResponse>();
+
+    await handler(req, res);
+    expect(res.statusCode).toBe(200);
+    expect(res._getJSONData()).toBeTruthy();
+  });
+});
