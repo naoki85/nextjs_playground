69bbe21a590b7b3586393d95d475b1c031787585
naoki85 <naoki.yoneyama.85@gmail.com>
2023-03-21 22:42:58 +0900

    test rules

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
-  const hoge = "fuga";
+  const hoge: any = "fuga";
   return (
     <div className={styles.container}>
       <Head>
