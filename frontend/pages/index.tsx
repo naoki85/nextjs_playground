ed8106cb940bc2745ab6f9120bf6a49ec3c0e72b
naoki85 <naoki.yoneyama.85@gmail.com>
2023-03-21 22:50:33 +0900

    reset invalid lines

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 
 const Home: NextPage = () => {
-  const hoge: any = "fuga";
   return (
     <div className={styles.container}>
       <Head>
