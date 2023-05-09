92e465654d7ab983f55a73cf15b162efcf884e11
naoki85 <naoki.yoneyama.85@gmail.com>
2023-05-09 09:31:03 +0900

    add error.tsx and update article list

--- nextjs-app-dir-example/src/app/page.tsx
+++ nextjs-app-dir-example/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Article } from '@/types';
+import ArticleList from "@/components/ArticleList";
 
 async function getArticles() {
   const res = await fetch("http://localhost:3000/api/articles", {
@@ -19,11 +20,7 @@ export default async function Home() {
   return (
     <div>
       <h1>新着記事</h1>
-      <ul>
-        {articles.map(article => (
-          <li key={article.id}>{article.title}</li>
-        ))}
-      </ul>
+      <ArticleList articles={articles} />
     </div>
   )
 }
